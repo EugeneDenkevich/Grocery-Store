4b8d886dee5b8cb35e615c5e684e6a25e8aff6e2
eugene <eugenestudio@mail.ru>
2023-06-21 09:49:11 +0300

    remove vercel

--- src/config/settings.py
+++ src/config/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'django-insecure-l^j)mp0(embdkh^v*n56(wv9(o_d3fir+#&1$-m=iynn06v+ck
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
